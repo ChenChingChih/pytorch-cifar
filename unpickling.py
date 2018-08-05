43e4975c190457d95ae7bebd52c6f4bb16c588ac
ChenChingChih <s410376056@gm.ntpu.edu.tw>
2018-08-05 10:59:12 +0800

    resnet110

--- unpickling.py
+++ unpickling.py
@@ -1,11 +1,11 @@
 import pickle
-with open("./resnet/resnet110_no_shortcut_epoch.txt", "rb") as fp:   # Unpickling 
+with open("./resnet/resnet110_epoch.txt", "rb") as fp:   # Unpickling 
   x= pickle.load(fp)
-with open("./resnet/resnet110_no_shortcut_loss.txt", "rb") as fp:   # Unpickling
+with open("./resnet/resnet110_loss.txt", "rb") as fp:   # Unpickling
   y= pickle.load(fp)
-with open("./resnet/resnet110_no_shortcut_epoch1.txt", "rb") as fp:   # Unpickling 
+with open("./resnet/resnet110_epoch1.txt", "rb") as fp:   # Unpickling 
   u= pickle.load(fp)
-with open("./resnet/resnet110_no_shortcut_acc.txt", "rb") as fp:   # Unpickling
+with open("./resnet/resnet110_acc.txt", "rb") as fp:   # Unpickling
   v= pickle.load(fp)
 print(x,y)
 print(u,v)
